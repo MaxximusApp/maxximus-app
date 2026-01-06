806c9f48eaa257b10b9983dfc92554923a3976dd
MaxximusApp <mkt.agency.ten@gmail.com>
2026-01-06 10:37:18 -0300

    Fix: Remove React dependencies and use Vue only

--- vite.config.js
+++ vite.config.js
@@ -1,36 +1,6 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
+import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico'],
-      manifest: {
-        name: 'Maxximus App',
-        short_name: 'Maxximus',
-        description: 'Aplicativo de desenvolvimento pessoal',
-        theme_color: '#0A0A0A',
-        icons: [
-          {
-            src: '/vite.svg',
-            sizes: '192x192',
-            type: 'image/svg+xml'
-          },
-          {
-            src: '/vite.svg',
-            sizes: '512x512',
-            type: 'image/svg+xml'
-          }
-        ]
-      }
-    })
-  ]
+  plugins: [vue()],
 })
-globIgnores: [
-  "**/node_modules/**/*",
-  "sw.js",
-  "workbox-*.js"
-]
